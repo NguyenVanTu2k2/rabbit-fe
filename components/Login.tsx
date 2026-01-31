ec1fbf1d139309f7ca7d33f4835ce52180b8166a
@lucodestudio-art <lucodestudio@gmail.com>
2026-01-31 16:06:10 +0700

    v1

--- components/Login.tsx
+++ components/Login.tsx
@@ -214,7 +214,7 @@ const Login: React.FC = () => {
           {/* Divider */}
           <div className="relative my-8">
             <div className="absolute inset-0 flex items-center">
-              <div className="w-full border-t border-gray-200"></div>
+              <div className="w-full border-t border-gray-200"> </div>
             </div>
             <div className="relative flex justify-center text-sm">
               <span className="px-3 bg-white text-gray-500 font-medium">Hoặc đăng nhập bằng</span>
